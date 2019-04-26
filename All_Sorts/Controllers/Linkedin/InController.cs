bf1e9122b746c00aec855c97e2974e40dcf73713
P0wd3r696 <saziyenge@gmail.com>
2019-04-26 10:43:23 +0200

    Simplified solution

--- All_Sorts/Controllers/Linkedin/InController.cs
+++ All_Sorts/Controllers/Linkedin/InController.cs
@@ -15,10 +15,7 @@ namespace All_Sorts.Controllers.Linkedin
         private string linkedinSharesEndpoint = "https://api.linkedin.com/v1/companies/18788968/shares?oauth2_access_token={0}";
         private const string defaultUrl = "https://localhost:447347/home";
         private const string defaultImageUrl = "https://localhost:447347/tomyimages";
-        private const string _accessToken = "AQUZubp9vSB5Ekf7wNOAwtXL1tqar9STKYTC7nLXDPLij4qFW3q5JU429AW7wvfO4tXXLA_Mthr6_" +
-            "-lEKjTWUB4W5yW3ie05q_WAmbpED4Jnuujk67Lz4tHSD_UpOukTG6lYZE9Px-QgdLEQ-Cnj8ENRXiDd_n4JBxPlcs1IxEkg-aEtjpOz1BKVcLFEHKos" +
-            "MUkXULZVmINA7q8RHPvYuCnnRTHHoxWv-rwtIWmM38XB290Wc4vkr_yOjTmJNIGu05bzbhaI88mt4_sdVI2kzsuZCg4wYhA49Sa1krlk0TWmK5TXlcjfs" +
-            "HKpvS3mR0RmUv9QeLFE8CTPRqYhM8D6FDZ3p4wEXQ";
+        private const string _accessToken = "";
 
         public bool PostToLinkedinAsync(string userText, string accessToken, string title = "Posted from All-Sortz App", string submittedUrl = defaultUrl, string submittedImageUrl= defaultImageUrl)
         {
--- All_Sorts/Controllers/PostsController.cs
+++ All_Sorts/Controllers/PostsController.cs
@@ -35,25 +35,22 @@ namespace All_Sorts.Controllers
         {
             public static string url = "https://localhost:44347/Home";
 
-            public static string accessToken = "AQUZubp9vSB5Ekf7wNOAwtXL1tqar9STKYTC7nLXDPLij4qFW3q5JU429AW7wvfO4tXXLA_Mthr6_-lEKjTWUB4W5yW3ie05q" +
-                "_WAmbpED4Jnuujk67Lz4tHSD_UpOukTG6lYZE9Px-QgdLEQ-Cnj8ENRXiDd_n4JBxPlcs1IxEkg-aEtjpOz1BKVcLFEHKosMUkXULZVmINA7q8RHPvYuCnnRTHHoxWv" +
-                "-rwtIWmM38XB290Wc4vkr_yOjTmJNIGu05bzbhaI88mt4_sdVI2kzsuZCg4wYhA49Sa1krlk0TWmK5TXlcjfsHKpvS3mR0RmUv9QeLFE8CTPRqYhM8D6FDZ3p4wEXQ";
+            public static string accessToken = "";
         }
         public static class FacebookSettings
         {
-            public static string pageID = "177502029051938";
+            public static string pageID = "";
 
-            public static string Access_Token = "EAAEAQCTS8w4BAERTIcBmpYyMRPQNnLCybBcoR5vlFhfVmIiIQD70tjNCGUqC0KW1ghgZBZBI3H5ufHXZBwZBsb6AKEeSTeaZAk5cZBkg5PJ" +
-                                                "ZCoZAn6AwJWxfLuIio31Ylsn4tynhoyKnMdVvZB3UvEWmjZCiYu7feGU32QrfQtQXTqCQZDZD";
+            public static string Access_Token = "";
 
             //public static string Access_Token = "EAAEAQCTS8w4BABnylvUl3T0kAHBJdZCgn8tFTZCquk9VwAFWz0c6uDT97Wt3ePzHdyC8wlcWiNoEs6bQZCLs6000GLPXMI11O1ZAs1TTnZAYEyyndfw82H1W5O311zoLCprlZBydilCZCWNPnAaiQUQCuK58xQQ6cUrALsh1jZCsJQZDZD";
         }
         public static class TwitterSettings
         {
-            public static string ConsumerKey = "qllUThirtgYjSoCKdvArwwKKR",
-                                ConsumerKeySecret = "GPiVBJ3NQ2o3yTgAKmllgfZVIcCqcNCje8HLZNflBP3MSnFU1h",
-                                AccessToken = "534605601-4rK4YOQxBJXCz8viVAliAQQ5c8pGLWkKEz7WkZ9Y",
-                                AccessTokenSecret = "H44h2jNrtEi6IvIeYkZdBmNWIuoyZrKMQG9Q9lSKHPQpn";
+            public static string ConsumerKey = "",
+                                ConsumerKeySecret = "",
+                                AccessToken = "",
+                                AccessTokenSecret = "";
         }
         //private static readonly HttpClient client = new HttpClient();
         private readonly ApplicationDbContext _db;
